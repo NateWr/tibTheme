3127c7358e9a538be9822f3113851062ac1932d9
Nate Wright <natew@notthisway.com>
2024-12-17 14:09:26 +0000

    Use different node versions for OJS and theme tests

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+sudo n 18.17.1
 npm install --prefix plugins/themes/tibTheme
 npm run build --prefix plugins/themes/tibTheme
 npx cypress run  --headless --browser chrome  --config integrationFolder=plugins/themes/tibTheme/cypress/tests
