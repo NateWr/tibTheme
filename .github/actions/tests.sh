0277d677b87b432696e1a0d50063887aadb90786
Nate Wright <natew@notthisway.com>
2024-12-17 12:19:09 +0000

    Fix: build assets before running tests

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+npm install --prefix plugins/themes/tibTheme
+npm run build --prefix plugins/themes/tibTheme
 npx cypress run  --headless --browser chrome  --config integrationFolder=plugins/themes/tibTheme/cypress/tests
